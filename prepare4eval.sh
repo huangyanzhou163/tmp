2f489d6fd2107f4abe05833021eee6e3a124edd7
huangyanzhou163 <huangyanzhou163@gmail.com>
2018-10-19 11:46:50 +0800

    add interactive interface

--- prepare4eval.sh
+++ prepare4eval.sh
@@ -3,14 +3,14 @@ model_dir="./projects/convai2/"
 src_dir="./Vsmn"
 unzip "Vsmn.zip"
 cat "data.tar.gz0" "data.tar.gz1" "data.tar.gz2" >  "data.tar.gz"
-tar -zxvf "data.tar.gz" -C ${src_dir}
-#unzip "data.zip" -d ${src_dir}
+tar zxvf "data.tar.gz" -C ${src_dir}
+cp "answer_sentence.pkl" ${src_dir}/data/
+cp "candidates.ann" ${src_dir}/data/
 if [ ! -d ${opt_dir} ];then
-    mkdir -p ${opt_dir} 
+    mkdir -p ${opt_dir}
 fi
 cp -r ${src_dir}/opt/Vsmn ${opt_dir}
 if [ ! -d ${model_dir} ];then
-    mkdir -p ${model_dir} 
+    mkdir -p ${model_dir}
 fi
 cp -r  ${src_dir} ${model_dir}
-
